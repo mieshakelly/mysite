41748268e3305f79f76c466d3da78bb5176bcf4d
Miesha Kelly <miesha@mieshakelly.design>
2021-05-25 07:38:48 +1000

    comment delete
    Deleted comment out of acupet template

--- acupet/templatetags/testnewfile.py
+++ acupet/templatetags/testnewfile.py
@@ -1,1 +1,5 @@
-# Put this back
+#Testing 123 in the templatetags folder
+#Miesha was here but now shes gone
+# sh=he left her name to turn you on 
+# those who knew her knew her well
+# those who didn't can go to hell 
