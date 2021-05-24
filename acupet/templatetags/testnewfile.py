5c69065df38b78cafcb26bfe82c712505172a724
mieshakelly <52680582+mieshakelly@users.noreply.github.com>
2021-05-24 22:28:39 +1000

    putting things back

--- acupet/templatetags/testnewfile.py
+++ acupet/templatetags/testnewfile.py
@@ -1,5 +1,1 @@
-#Testing 123 in the templatetags folder
-#Miesha was here but now shes gone
-# sh=he left her name to turn you on 
-# those who knew her knew her well
-# those who didn't can go to hell 
+# Put this back
